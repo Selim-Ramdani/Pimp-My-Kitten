efa54dc51c7750bc90da90b579afc0b0fb339c8e
curlyroots <selimcodeur@gmail.com>
2022-03-13 17:28:16 +0100

    migration and model user change + active_storage installed

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+         
+  has_one_attached  :avatar
+
+  validates_presence_at :first_name, last_name
+end
