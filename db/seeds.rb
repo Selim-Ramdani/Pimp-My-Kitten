a6bc6719ae3876cf0bf88c47572d4dc9b8daccac
Sarah Coppin <sacoppin@hotmail.com>
2022-03-14 16:27:55 +0100

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Addresse.destroy_all
+Cart.destroy_all
+Item.destroy_all
+Product.destroy_all
+User.destroy_all
+
+
+######### - Admin user - #########
+User.create!(
+    email: "kitten.admin@yopmail.com",
+    password: "miaou",
+    is_admin: true
+  )
+  puts "1 admin created"
