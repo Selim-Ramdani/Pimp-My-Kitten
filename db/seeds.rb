4e6d5c4159dc90168cc4789aad76a5abce672e85
curlyroots <selimcodeur@gmail.com>
2022-03-16 15:52:35 +0100

    table order + model  and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,56 +7,55 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Address.destroy_all
-Cart.destroy_all
-Item.destroy_all
-Product.destroy_all
-User.destroy_all
-AdminUser.destroy_all
+10.times do |i|
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: 'user-mail' + '@yopmail.com',
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
+10.times do 
+  Product.create(
+    title: Faker::Creature::Cat.unique.name,
+    description: 'Lorem ipsum ...',
+    price: rand(10...250)
+  )
+end
 
-#10.times do |i|
-#   User.create(
-#     first_name: Faker::Name.first_name,
-#     last_name: Faker::Name.last_name,
-#     email: 'user-mail' + '@yopmail.com',
-#     password: 'password',
-#     password_confirmation: 'password'
-#   )
-# end
-# 10.times do 
-#   Product.create(
-#     title: Faker::Creature::Cat.unique.name,
-#     description: 'Lorem ipsum ...',
-#     price: rand(10...250)
-#   )
-# end
-
-# 10.times do 
-#   Cart.create(
-#     user_id: User.all.sample..id,
-#   )
-# end
+10.times do 
+  Cart.create(
+    user_id: User.all.sample.id
+  )
+end
 
 
-# 10.times do |i|
-#   Item.create(
-#      product_id: Product.all.sample.id,
-#     cart_id: Card.all.sample.id,
-#     quantity: rand(10...250)
-#   )
-# end
+10.times do |i|
+  Item.create(
+     product_id: Product.all.sample.id,
+    cart_id: Cart.all.sample.id,
+    quantity: rand(10...250)
+  )
+end
+
+10.times do |i|
+  Order.create(
+     product_id: Product.all.sample.id,
+     user_id: User.all.sample.id
+  )
+end
 
 
-
-# 10.times do |i|
-#   Address.create(
-#     # user_id: User.all.sample.id,
-#     street_number: Faker::Address.building_number,
-#     street_name: Faker::Address.street_name,
-#     zipcode: Faker::Address.zip_code,
-#     city: Faker::Address.city,
-#     country: Faker::Address.country
-#   )
-# end
+10.times do |i|
+  Address.create(
+    # user_id: User.all.sample.id,
+    street_number: Faker::Address.building_number,
+    street_name: Faker::Address.street_name,
+    zipcode: Faker::Address.zip_code,
+    city: Faker::Address.city,
+    country: Faker::Address.country
+  )
+end
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
