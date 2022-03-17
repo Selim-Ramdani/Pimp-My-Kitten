783ada9a5ea3d925c8496cd04e1525d4e599af36
Sarah Coppin <sacoppin@hotmail.com>
2022-03-17 11:15:12 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,4 +53,4 @@ end
   )
 end
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
